cd7a1a018b6923000d338bc4a5f8dfc72bbc6ba8
Vitaliy <95574118+mgpwnz@users.noreply.github.com>
2022-10-30 15:07:16 +0200

    Update sui_update.sh

--- sui_update.sh
+++ sui_update.sh
@@ -30,4 +30,3 @@ mv ~/sui/target/release/sui-node /usr/local/bin/
 mv ~/sui/target/release/sui /usr/local/bin/
 wget -O /var/sui/genesis.blob https://github.com/MystenLabs/sui-genesis/raw/main/devnet/genesis.blob
 systemctl restart suid.service
-rm $HOME/sui_update.sh
