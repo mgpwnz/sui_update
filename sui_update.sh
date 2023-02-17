b4f103e4431281ff2362149c87b3a7fe7deaeba0
Vitaliy <95574118+mgpwnz@users.noreply.github.com>
2023-02-17 12:23:06 +0200

    Update sui_update.sh

--- sui_update.sh
+++ sui_update.sh
@@ -25,7 +25,7 @@ cd sui
 #git remote add upstream https://github.com/MystenLabs/sui
 #git fetch upstream
 #git checkout -B devnet --track upstream/devnet
-git checkout devnet-0.24.0
+git checkout devnet-0.26.1
 #cargo build -p sui-node -p sui --release
 cargo build --bin sui-node --bin sui --release
 mv ~/sui/target/release/sui-node /usr/local/bin/
